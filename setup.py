7955b5ea48c141c49f6c0464dc72524eeac6bf30
Loren Kuich <loren.jk3@gmail.com>
2019-07-17 22:14:10 -0700

    Added requirements

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lkuich/coach-python",
     packages=setuptools.find_packages(),
+    install_requires=['tensorflow==1.12', 'requests', 'numpy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
