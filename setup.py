d35de9283d238e58d98458e870a5a0eb8db8e704
Loren Kuich <loren.jk3@gmail.com>
2019-09-18 16:40:54 -0700

    Updated to 0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="coach-ml",
-    version="0.12",
+    version="0.13",
     author="Loren Kuich",
     author_email="loren@lkuich.com",
     description="Python client for coach",
